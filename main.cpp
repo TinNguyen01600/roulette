61f89bf74a75648c45c2f6c68ad0c428d0abd3ef
unknown <tinnguyen01600@gmail.com>
2021-12-14 05:22:10 +0200

    delete some unused comment

--- main.cpp
+++ main.cpp
@@ -2,12 +2,9 @@
 
 int main(){
     system("cls");
-    //welcome();
 
     Number *number = new Number[36];
     initialize(number);
     print_roulette_table(number);
-
-    //number[33].print_data();
     
 }
