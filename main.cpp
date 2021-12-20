6f9d5e1c7dcdeaa350044787a73de19b35dc90f5
unknown <tinnguyen01600@gmail.com>
2021-12-20 13:54:00 +0200

    add table border for roulette table

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 
 int main(){
     system("cls");
-
+    welcome();
     Number *number = new Number[36];
     initialize(number);
     print_roulette_table(number);
-    
 }
--- print.cpp
+++ print.cpp
@@ -2,47 +2,51 @@
 
 void welcome()
 {   
-    gotoxy(38,0);
+    gotoxy(18,1);
     set_color(light_blue); cout << "****************";
 
-    gotoxy(34,1);
+    gotoxy(14,2);
     set_color(cyan); cout << "------ ";
     set_color(yellow); cout<< "Welcome to ";
     set_color(cyan); cout << "-------";
     
-    gotoxy(29,2);
+    gotoxy(9,3);
     set_color(light_blue);  cout << "******** ";
     set_color(yellow); cout << "RED ROCK CASINO ";
     set_color(light_blue); cout << "**********";
 
-    gotoxy(32,3);
+    gotoxy(12,4);
     set_color(cyan); cout << "-----------------------------";
 
-    gotoxy(34,4);
+    gotoxy(14,5);
     set_color(light_blue); cout << "*************************" ;
 
-    gotoxy(37,5);
+    gotoxy(17,6);
     set_color(cyan); cout << "*******************" ;
 
-    gotoxy(24,7);
+    gotoxy(4,8);
     set_color(13); cout << "~~~~~~~~~~~~~~~ Roulette Table ~~~~~~~~~~~~~~~" << endl << endl;
-    set_color(white);
-
     set_color(white);
 }
 
 void print_roulette_table(Number *number)
 {
     int i, j, k;
+    cout << " ________________________" << endl;
+    cout << "|" << endl;
     for (i = 0; i<3; i++){
+        cout << "|     ";
         for (j  = 0; j<12; j++){
             k = i+3*j;
             number[k].print_data();
-            cout << "   ";
+            cout << "      ";
         }
-        cout << endl;
+        cout << "|" << endl;
     }
+    //cout << "|________________|___________________|___________________|" << endl;
 
-
-    set_color(white);
+    // cout << "|                |                   |                   |" << endl;
+    // cout << "|    1 - 12      |      13 - 24      |      25 - 36      |" << endl;
+    // cout << "|   (Area 1)     |      (Area 2)     |      (Area 3)     |" << endl;
+    // cout << "|________________|___________________|___________________|";
 }
