9a73fb2a3705bdd8d39a0fc3c20b03907ceeb08d
unknown <tinnguyen01600@gmail.com>
2021-12-20 14:39:29 +0200

    add area 1 to roulette table

--- print.cpp
+++ print.cpp
@@ -2,38 +2,47 @@
 
 void welcome()
 {   
-    gotoxy(18,1);
+    gotoxy(40,1);
     set_color(light_blue); cout << "****************";
 
-    gotoxy(14,2);
+    gotoxy(36,2);
     set_color(cyan); cout << "------ ";
     set_color(yellow); cout<< "Welcome to ";
     set_color(cyan); cout << "-------";
     
-    gotoxy(9,3);
+    gotoxy(31,3);
     set_color(light_blue);  cout << "******** ";
     set_color(yellow); cout << "RED ROCK CASINO ";
     set_color(light_blue); cout << "**********";
 
-    gotoxy(12,4);
+    gotoxy(34,4);
     set_color(cyan); cout << "-----------------------------";
 
-    gotoxy(14,5);
+    gotoxy(36,5);
     set_color(light_blue); cout << "*************************" ;
 
-    gotoxy(17,6);
+    gotoxy(39,6);
     set_color(cyan); cout << "*******************" ;
 
-    gotoxy(4,8);
+    gotoxy(26,8);
     set_color(13); cout << "~~~~~~~~~~~~~~~ Roulette Table ~~~~~~~~~~~~~~~" << endl << endl;
     set_color(white);
 }
 
+
 void print_roulette_table(Number *number)
 {
     int i, j, k;
-    cout << " ________________________" << endl;
+    cout << " ";
+    for (i = 0; i<98; i++){
+        cout << "_";
+    }
+    cout << endl << "|";
+    for (i = 0; i<98; i++){
+        cout << " ";
+    }
     cout << "|" << endl;
+//----------------------------------------------------------------------------------
     for (i = 0; i<3; i++){
         cout << "|     ";
         for (j  = 0; j<12; j++){
@@ -43,10 +52,22 @@ void print_roulette_table(Number *number)
         }
         cout << "|" << endl;
     }
-    //cout << "|________________|___________________|___________________|" << endl;
-
-    // cout << "|                |                   |                   |" << endl;
-    // cout << "|    1 - 12      |      13 - 24      |      25 - 36      |" << endl;
-    // cout << "|   (Area 1)     |      (Area 2)     |      (Area 3)     |" << endl;
-    // cout << "|________________|___________________|___________________|";
+//----------------------------------------------------------------------------------
+    for (i = 0; i<3; i++){
+        cout << "|";
+        for (j = 0; j<31; j++){
+            cout << "_";
+        }
+    }
+    cout << "___|" << endl;
+//----------------------------------------------------------------------------------
+    for (i = 0; i<3; i++){
+        cout << "|";
+        for (j = 0; j<31; j++){
+            cout << " ";
+        }
+    }
+    cout << "   |" << endl;
+//----------------------------------------------------------------------------------
+    cout << "|\t     1 - 12\t\t|";
 }
