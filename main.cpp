901f3a1f3ddb8778afd53139941e91ec432a291f
TinNguyen01600 <ductin16111@gmail.com>
2021-12-28 16:09:07 +0200

    update print_roulette_table() function

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(){
     system("cls");
-    system("MODE 90,25");
+    system("MODE 90,25");   //change the cmd size to 90,25
     welcome();
     Number *number = new Number[36];
     initialize(number);
